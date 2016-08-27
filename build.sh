4babcac9923b46afaaa844ca26ab5603b5a950fc
Lukasz Rozycki <git@lukaszr.com>
2016-08-27 16:05:44 -0600

    Download Hugo to temporary directory. Deleted .drone.yml (drone.io and opensource drone are separate projects

--- build.sh
+++ build.sh
@@ -11,12 +11,13 @@ git submodule update --init --recursive
 
 # download hugo 0.16 release
 HUGO=0.16_linux-64bit
-curl -L -o /tmp/hugo_$HUGO.tgz https://github.com/spf13/hugo/releases/download/v0.16/hugo_$HUGO.tgz
-tar xvf /tmp/hugo_$HUGO.tgz -C /tmp/hugo_$HUGO
-mv /tmp/hugo_$HUGO/hugo /usr/bin/hugo
+DIR=`mktemp -d`
+
+curl -L -o $DIR/hugo_$HUGO.tgz https://github.com/spf13/hugo/releases/download/v0.16/hugo_$HUGO.tgz
+tar xvf $DIR/hugo_$HUGO.tgz -C $DIR
 
 # build the static web content
-hugo
+$DIR/hugo
 
 # push the static web content to gh-pages
 ./deploy.sh
